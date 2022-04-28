825338e0ce9acf3e335627a3aae5d380102a9265
Artur Alcoverro <artur.bcn1998@gmail.com>
2022-04-28 11:45:22 +0200

    -

--- js/index.js
+++ js/index.js
@@ -11,12 +11,13 @@ loadProjects()
 lock()
 
 let vh = (window.innerHeight - 0.1) * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 window.onresize = responsiveImage
 
 function updateVh(){
     vh = (window.innerHeight - 0.1) * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`)
-    document.body.style.minHeight = "100vh";
+    document.body.style.minHeight = "calc(var(--vh, 1vh) * 100)";
     setTimeout(() => {
         document.body.style.minHeight = "-webkit-fill-available";   
     }, 100);
