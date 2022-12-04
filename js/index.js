311d50bbb8e1f23b8cb8bb6d4b4ae29be1ba9ea9
ArturAlcoverro <artur.bcn1998@gmail.com>
2022-12-04 12:29:56 +0100

    z-index carrousel

--- js/index.js
+++ js/index.js
@@ -270,8 +270,6 @@ function unlock() {
     for (let e of projecstInfo) {
         e.classList.add("difference")
     }
-
-    refreshHiddenProjectsPosition()
 }
 
 function log(text) {
@@ -282,18 +280,4 @@ function log(text) {
 
 function clearLog() {
     // document.getElementById("data").innerHTML = '';
-}
-
-function refreshHiddenProjectsPosition() {
-    let elements = document.querySelectorAll('.project');
-
-    elements.forEach(function (element) {
-        element.style.left = '300%';
-    });
-
-    setTimeout(() => {
-        elements.forEach(function (element) {
-            element.style.left = '';
-        });
-    }, 5000);
 }
